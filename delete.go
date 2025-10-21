1f08c6e3cc5a84c188f40335fe59793cfa817869
areyes-bsale <areyes-bsale@imaginex.cl>
2025-10-21 08:22:34 -0300

    examples as a code doc blocks to increase coverage

--- delete.go
+++ delete.go
@@ -6,6 +6,28 @@ type DeleteStm struct {
 	*stament
 }
 
+// Delete initializes a new DeleteStm instance.
+//	fmt.Println("Simple Delete")
+//	q, p := v2.Delete().From("client").Build()
+//	fmt.Printf("query: %s \nparams: %v\n\n", q, p)
+//	OUTPUT:
+//	query: DELETE FROM client
+//	params: []
+
+// Delete with conditions
+//
+//	fmt.Println("Delete With conditions")
+//	q, p = v2.Delete().From("client").
+//		Where("client_id = 100").
+//		And("estado_cliente = 0").
+//		Y().In("regime_cliente", "'G01','G02', ?", "'G03'").
+//		And("a").
+//		Build()
+//
+//	fmt.Printf("query: %s \nparams: %v\n\n", q, p)
+//	OUTPUT:
+//	query: DELETE FROM client WHERE client_id = 100 AND estado_cliente = 0 AND regime_cliente IN ('G01','G02', ?) AND a
+//	params: ['G03']
 func Delete() *DeleteStm {
 	d := &DeleteStm{
 		stament: pool.Get().(*stament),
